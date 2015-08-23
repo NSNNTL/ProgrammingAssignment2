c45636bbb9567f4c72e928b8bac7da9cd5a6e9f6
NSNNTL <nsnntl@gmail.com>
2015-08-23 14:43:26 -0400

    revise r

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Caching the Inverse of a Matrix:
+## This R program is used for Caching the Inverse of a Matrix:
 ## Matrix inversion is usually a costly computation and there may be some 
 ## benefit to caching the inverse of a matrix rather than compute it repeatedly.
 ## Below are a pair of functions that are used to create a special object that 
@@ -39,3 +39,4 @@ cacheSolve <- function(x, ...) {
   x$setInverse(inv)
   inv
 }
+
